bf704fb6b0959aa7cb45932d5f4c913280242b1b
Gabriel Guerrero <gabe.guerrero@gmail.com>
2021-09-11 15:44:09 -0500

    Create loop to print differences in files for observation.

--- RenameFile.py
+++ RenameFile.py
@@ -1,22 +1,22 @@
 def printFiles():
-    infile=open('myfile.txt','r')
+    infile=open('correct.txt','r')
     line=infile.readline()
     lineList=[]
     while line !='':
-        print(line)
         line=infile.readline()
         lineList.append(line)
     infile.close()
-    infile2=open('correct.txt','r')
+    infile2=open('myfile.txt','r')
     line2=infile2.readline()
     lineList2=[]
     while line2 !='':
-        print(line2)
         line2=infile2.readline()
         lineList2.append(line2)
     infile2.close()
-    if lineList in lineList2:
-        print("True")
-    else:
-        print("False")
+    for x in range(len(lineList)):
+        if lineList[x] == lineList2[x]:
+            print('Line>', x+1 , '- same')
+        elif lineList[x] != lineList2[x]:
+            print('Line>', x+1, 'in myfile is different\n', '\n       CORRECT>>\n',
+                  repr(lineList[x]), '\n       MYFILE>>\n', repr(lineList2[x]), '\n')
 printFiles()
